f5211d9b51020ca08e8b3213c8314ada5fbc6de2
Jack Zampolin <jack.zampolin@gmail.com>
2017-07-06 13:26:42 -0700

    versions

--- install_tick.sh
+++ install_tick.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+INFLUXDB_VERSION="1.2.4"
+KAPACITOR_VERSION="1.3.2"
+TELEGRAF_VERSION="1.3.1"
+CHRONOGRAF_VERSION="1.3.3.1"
+
+# Install Dependancies 
 sudo apt-get update && sudo apt-get install wget
 
-# First install each of the components
-# Each comes with systemd unit files to manage
-
 # Install InfluxDB
-wget https://dl.influxdata.com/influxdb/releases/influxdb_1.2.4_amd64.deb
-sudo dpkg -i influxdb_1.2.4_amd64.deb
+wget https://dl.influxdata.com/influxdb/releases/influxdb_$(INFLUXDB_VERSION)_amd64.deb
+sudo dpkg -i influxdb_$(INFLUXDB_VERSION)_amd64.deb
 
 # Install Telegraf
-wget https://dl.influxdata.com/telegraf/releases/telegraf_1.3.2-1_amd64.deb
-sudo dpkg -i telegraf_1.3.2-1_amd64.deb
+wget https://dl.influxdata.com/telegraf/releases/telegraf_$(TELEGRAF_VERSION)-1_amd64.deb
+sudo dpkg -i telegraf_$(TELEGRAF_VERSION)-1_amd64.deb
 
 # Install Kapacitor
-wget https://dl.influxdata.com/kapacitor/releases/kapacitor_1.3.1_amd64.deb
-sudo dpkg -i kapacitor_1.3.1_amd64.deb
+wget https://dl.influxdata.com/kapacitor/releases/kapacitor_$(KAPACITOR_VERSION)_amd64.deb
+sudo dpkg -i kapacitor_$(KAPACITOR_VERSION)_amd64.deb
 
 # Install Chronograf
-wget https://dl.influxdata.com/chronograf/releases/chronograf_1.3.3.1_amd64.deb
-sudo dpkg -i chronograf_1.3.3.1_amd64.deb
+wget https://dl.influxdata.com/chronograf/releases/chronograf_$(CHRONOGRAF_VERSION)_amd64.deb
+sudo dpkg -i chronograf_$(CHRONOGRAF_VERSION)_amd64.deb
 
 # Delete the debs after installation
 sudo rm *.deb
